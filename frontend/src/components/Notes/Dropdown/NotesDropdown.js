1678e61019a9b4d4a82566547261497e9e4490a7
Patrick Choi <ptrckchoi@gmail.com>
2023-02-06 03:22:14 -0800

    will start comments

--- frontend/src/components/Notes/Dropdown/NotesDropdown.js
+++ frontend/src/components/Notes/Dropdown/NotesDropdown.js
@@ -1,14 +1,26 @@
 
-
+import {useState} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import LikePostItem from '../Likes/LikePostItem';
 const NotesDropdown = ({likes}) => {
   const dispatch = useDispatch();
-  
+  const [showLikes, setShowLikes] = useState(true)
+  const closeLikes = () => {
+    setShowLikes(false)
+  }
+  const openLikes = () => {
+    setShowLikes(true)
+  }
+    
   return (
-    <div>
-      {likes.map(like => <LikePostItem key={like.id} postId={like.postId} userId={like.userId} />)}
+    <div className="notes-dropdown">
+      <div className="notes-dropdown-header">
+        <button onClick={openLikes}>Likes</button>
+      </div>
+      <div className="notes-dropdown-content">
+        {showLikes && likes.map(like => <LikePostItem key={like.id} postId={like.postId} userId={like.userId} />)}
+      </div>
     </div>
   )
 }
